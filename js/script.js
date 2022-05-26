86e0eead8a84ed6fe79be4caedc6c48fbb0b3844
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-05-26 15:38:59 +0200

    finalizzazione milestone 1, 2, 3

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const listToDo = new Vue({
+    el : '#app',
+    data : {
+        newTask : "",
+        toDo : [
+            {
+                text : 'fare il bucato',
+                done : false
+            },
+            {
+                text : 'pulire il pavimento',
+                done : true
+            },
+            {
+                text : 'Cucinare',
+                done : false
+            },
+            {
+                text : 'studiare',
+                done : true
+            },
+            {
+                text : 'andare a fare la spesa',
+                done : false
+            },
+        ]
+    },
+    methods : {
+        addTask(){
+            // creazine del nuovo oggetto
+            const task = new Object();
+            task.text = this.newTask;
+            task.done = true;
+            // stampa solo se l'input non è vuoto
+            if(this.newTask !== ""){
+                this.toDo.push(task);
+                this.newTask = "";
+            }
+        },
+        removeTask(task){
+            this.toDo.splice(task, 1);
+        },  
+
+        log(){
+            console.log(this.newTask);
+        
+        }   
+    }
+    
+});
+
